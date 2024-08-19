b03af4971d3a6d61c2b32435b051dee5ea6aea1b
Archery Luna <76516425+ArcheryLuna@users.noreply.github.com>
2024-08-19 19:18:11 +0100

    pass 1st stage

--- app/main.ts
+++ app/main.ts
@@ -4,8 +4,8 @@ import * as net from "net";
 console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
-// const server: net.Server = net.createServer((connection: net.Socket) => {
-//   // Handle connection
-// });
-//
-// server.listen(6379, "127.0.0.1");
+const server: net.Server = net.createServer((connection: net.Socket) => {
+// Handle connection
+});
+
+server.listen(6379, "127.0.0.1");
