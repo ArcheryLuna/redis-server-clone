dba5d0a4f82cfc8ad386853c59157697dec55e7c
Archery Luna <76516425+ArcheryLuna@users.noreply.github.com>
2024-08-19 19:24:48 +0100

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -6,6 +6,11 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
 // Handle connection
+    connection.on("data", (data: any) => {
+        console.log(`Recieved Request : ${JSON.stringify(data.toString())}`);
+        connection.write("+PONG\r\n");    
+    })
+
 });
 
 server.listen(6379, "127.0.0.1");
