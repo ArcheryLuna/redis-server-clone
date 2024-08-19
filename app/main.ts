0892092c5e4cec5b022792a5e88c1b885dfcb880
Archery Luna <76516425+ArcheryLuna@users.noreply.github.com>
2024-08-19 20:07:12 +0100

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -6,8 +6,29 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
 // Handle connection
-    connection.on("data", (data: any) =>  {
-        connection.write("+PONG\r\n")
+    connection.on("data", (data: Object) =>  {
+
+        // Take data create array 0 is command anything after 0 is the args
+        // Take out all the unessisary data
+        // parse the command via a switch statement
+
+        console.log(JSON.stringify(data.toString()))
+
+        const command = data.toString().split("\r\n").filter((line) => 
+            !line.startsWith("*") && 
+            !line.startsWith("$") && 
+            line !== ""
+        );
+
+        // console.log(command);
+        
+        switch(command[0].toLowerCase()) {
+            case "echo":
+                connection.write(`$${command[1].length}\r\n${command[1]}\r\n`);
+                break;
+            case "ping":
+                connection.write(`+PONG\r\n`)
+        }
     })
 
 });
